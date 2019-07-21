3e644af324d9cfc61df72b29391e0c1990fd78cd
amasomo10 <gutenfair@gmail.com>
2019-07-21 00:44:22 -0500

    Homepage created and empty pages

--- psite/views.py
+++ psite/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'psite/home.html', {})
+
+def about(request):
+    return render(request, 'psite/about.html', {})
+
+def projects(request):
+    return render(request, 'psite/projects.html', {})
+
+def calculation(request):
+    return render(request, 'psite/calculation.html', {})
